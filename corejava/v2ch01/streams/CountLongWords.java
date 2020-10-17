00bf0e124c979e60bf05b3e11665dd5c9da6f7fd
blue_raincoat <h199605342gmail.com>
2020-10-17 15:12:09 +0800

    #

---
+++ corejava/v2ch01/streams/CountLongWords.java
@@ -0,0 +1,28 @@
+package v2ch01.streams;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.*;
+
+public class CountLongWords
+{
+    public static void main(String[] args) throws IOException
+    {
+        String contents = new String(Files.readAllBytes(
+                Paths.get("D:\\corejava\\gutenberg\\alice30.txt")), StandardCharsets.UTF_8
+        );
+        List<String> words = Arrays.asList(contents.split("\\PL+"));
+
+        long count = 0;
+        for (String w : words) if (w.length() > 12) count++;
+        System.out.println(count);
+
+        count = words.stream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+
+        count = words.parallelStream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+    }
+}
