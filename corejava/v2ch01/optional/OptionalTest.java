d74febf98772b7e93711c9e39281f7aaf35b53a5
blue_raincoat <h19960534@gmail.com>
2020-10-19 11:19:13 +0800

    finish OptionalTest

--- corejava/v2ch01/optional/OptionalTest.java
+++ corejava/v2ch01/optional/OptionalTest.java
@@ -9,11 +9,13 @@ public class OptionalTest
 {
     public static void main(String[] args) throws IOException
     {
+        Path path = Paths.get("D:\\corejava\\gutenberg\\alice30.txt");
+        String contents = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
         List<String> wordList = Arrays.asList(contents.split("\\PL+"));
 
         Optional<String> optionalValue = wordList.stream()
-                .filter(s -> s.contains("fred")).findFirst();
-        System.out.println(optionalValue.orElse("No word") + " contains fred");
+                .filter(s -> s.contains("is")).findFirst();
+        System.out.println(optionalValue.orElse("No word") + " contains is");
 
         Optional<String> optionalString = Optional.empty();
         String result = optionalString.orElse("N/A");
@@ -34,6 +36,24 @@ public class OptionalTest
         optionalValue.ifPresent(s -> System.out.println(s + " contains red"));
 
         Set<String> results = new HashSet<>();
+        optionalValue.ifPresent(results::add);
+        Optional<Boolean> added = optionalValue.map(results::add);
+        System.out.println(added);
 
+        System.out.println(inverse(4.0).flatMap(OptionalTest::squareRoot));
+        System.out.println(inverse(-1.0).flatMap(OptionalTest::squareRoot));
+        System.out.println(inverse(0.0).flatMap(OptionalTest::squareRoot));
+        Optional<Double> result2 = Optional.of(-4.0).flatMap(OptionalTest::squareRoot);
+        System.out.println(result2);
+    }
+
+    public static Optional<Double> inverse(Double x)
+    {
+        return x == 0 ? Optional.empty() : Optional.of(1 / x);
+    }
+
+    public static Optional<Double> squareRoot(Double x)
+    {
+        return x < 0 ? Optional.empty() : Optional.of(Math.sqrt(x));
     }
 }
