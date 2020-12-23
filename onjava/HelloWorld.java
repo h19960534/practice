7da2fcb1650bdb6c82d42132126a032638cfb863
blue_raincoat <h199605342gmail.com>
2020-12-23 22:05:14 +0800

    Stats.java

--- onjava/HelloWorld.java
+++ onjava/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
+
         public static void main(String[] args){
-            System.out.println("你好世界");
+            byte a = 127;
+            byte b = 1;
+            byte c = (byte)(a + b);
+            System.out.println("你好世界" + c);
+            int[] abc = new int[]{1,2,3};
         }
     }   
